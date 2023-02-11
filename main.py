ae60d2cf9c3e1e2c230fd9d4f2242370455d0f27
Ryleigh turcotte <124654694+Nedstark912@users.noreply.github.com>
2023-02-11 12:15:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -220,9 +220,9 @@ async def start(_: pbot, m: Message):
 You just need to send me the message containing the links! I will replace the un shorted link and send you back!
 Do wait for 10 seconds for each link to process and bypass after sending the links!
 
-**Support: @GreyMatter_Bots!**
+**Support: @Magnus741!**
 
-**Subscribe: https://youtube.com/@GreyMattersYT**""")
+**Subscribe: https://t.me/redoxofficially**""")
     return
 
 
